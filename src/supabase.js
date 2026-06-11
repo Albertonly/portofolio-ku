f1ca53f4d8a962a39163cdec5dfa97445976c0a4
Albertonly <115127359+Albertonly@users.noreply.github.com>
2026-06-11 18:26:19 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Ganti URL dan KEY di bawah dengan milikmu dari Dasbor Supabase
-const supabaseUrl = 'https://bmjflatpnowbwgbsfsre.supabase.co/rest/v1/'
+// Cukup masukkan URL base saja tanpa /rest/v1/
+const supabaseUrl = 'https://bmjflatpnowbwgbsfsre.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtamZsYXRwbm93YndnYnNmc3JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNTc3NzcsImV4cCI6MjA5NjczMzc3N30.0yaqMRSzFmDAjbqlzuBemINhV5nc9dCFEfIrr2a11A0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
